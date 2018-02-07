43df31c9d8815083aac4f7e1c709a04d3cb3fd16
Howard Ju <howard.ju@tvnz.co.nz>
2018-02-07 18:59:09 +1300

    Hide the frame and set the title bar drag-able

--- src/js/css-injector.js
+++ src/js/css-injector.js
@@ -10,6 +10,17 @@ CssInjector.main = `
     ._254GqExCxnOxmPy7kKATP2 {
         display: none !important;
     }
+
+    ._2SETNF5hlFf4JfNCfzGXnh {
+        padding-top: 30px !important;
+        -webkit-app-region: drag;
+    }
+
+    .iAvecXA_GRQH_AAGS1qhn._2SvWF44TZgwj95spKJn8Za,
+    .kX6zr2GeZd6pqWAmijYlZ,
+    .ms-FocusZone._2heo7Eue0oDeyWNI4XaGWZ {
+        -webkit-app-region: no-drag;
+    }
 `
 
 module.exports = CssInjector
