3182664460fc3074eb1bae0b646eaca1c0e517e7
Howard Ju <howard.ju@tvnz.co.nz>
2018-03-21 15:16:52 +1300

    shhhh....

--- src/js/css-injector.js
+++ src/js/css-injector.js
@@ -3,22 +3,31 @@ class CssInjector {
 }
 
 CssInjector.main = `
+    /* hide the vertical ad bar */
     ._1K0cujP_EBzCd77bTesW1q {
         display: none !important;
     }
 
+    /* hide the small ad bar in other email page */
     .mnkcvmImptYPIG0m29FJh {
         display: none !important;
     }
 
-    ._2SETNF5hlFf4JfNCfzGXnh {
+    /* hide the upgrade premium ad bar */
+    ._1x0NRQ4xIoC0ZaTr2yjnJc {
+        display: none !important;
+    }
+
+    /* make the header higher and dragable */
+    ._3fpgaLm7NwXSzB_ETFcCNj {
         padding-top: 30px !important;
         -webkit-app-region: drag;
     }
 
-    .iAvecXA_GRQH_AAGS1qhn._2SvWF44TZgwj95spKJn8Za,
-    .kX6zr2GeZd6pqWAmijYlZ,
-    .ms-FocusZone._2heo7Eue0oDeyWNI4XaGWZ {
+    /* make the clickable component in header not dragable */
+    ._3nKSQcmZjHV3x2jl9xjqWj._2S-r1xigq2rKITZQs-EiCV,
+    ._3w0vuS2NulbHxhF122Um_L,
+    .ms-FocusZone._2UyyMXJKZ_eKg5jzcJFAbN {
         -webkit-app-region: no-drag;
     }
 `
