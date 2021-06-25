7cca36df48cdb50a79cf7b18bd233c4314b54dd5
David Haukeness <david@hauken.us>
2021-06-25 14:24:25 -0600

    use isVisible() and change label to reflect (#130)

--- src/controller/tray-controller.js
+++ src/controller/tray-controller.js
@@ -17,7 +17,7 @@ class TrayController {
         this.tray = new Tray(this.createTrayIcon(''))
 
         const context = Menu.buildFromTemplate([
-            {label: 'Show Me', click: () => this.showHide()},
+            {label: 'Show/Hide', click: () => this.showHide()},
             {label: 'Separator', type: 'separator'},
             {label: 'Window Frame', type: 'checkbox', checked: settings.get('showWindowFrame', true), click: () => this.toggleWindowFrame()},
             {label: 'Quit', click: () => this.cleanupAndQuit()}
