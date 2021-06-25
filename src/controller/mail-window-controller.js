1ebdc1685ef33705df711e1512a66c080bfd4c9b
David Haukeness <david@hauken.us>
2021-06-25 14:24:25 -0600

    use isVisible() and change label to reflect (#130)

--- src/controller/mail-window-controller.js
+++ src/controller/mail-window-controller.js
@@ -135,7 +135,7 @@ class MailWindowController {
     }
 
     toggleWindow() {
-        if (this.win.isFocused()) {
+        if (this.win.isVisible()) {
             this.win.hide()
         } else {
             this.show()
